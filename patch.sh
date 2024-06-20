f093700f70eef9a8bc944f0bd7525624f77d9f51
hoysong_firebat <hoysong@student.42gyeongsan.kr>
2024-06-21 01:44:17 +0900

    term auto enable

--- patch.sh
+++ patch.sh
@@ -1,9 +1,6 @@
 echo "clone vundlevim"
-#git clone git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "make new vimrc file"
-
-rm -rf ~/.vimrc
 echo "if has(\"systax\")
     syntax on
 endif
@@ -17,15 +14,13 @@ set hlsearch
 set nu
 \" Enable copy to clipboard
 set clipboard=unnamed
+set termwinsize=7x200 \" vim 안에서 ':term' 명령을 통해 생성된 터미널의 크기입니다. (수직)x(수평)
 
 filetype plugin indent on    \" required
 set nocompatible              \" be iMproved, required
 filetype off                  \" required
 
-
 set rtp+=~/.vim/bundle/Vundle.vim \" set the runtime path to include Vundle and initialize
-
-\"plugin begin code==============================================
 call vundle#begin()
 Plugin 'VundleVim/Vundle.vim' \"let Vundle manage Vundle, required
 \" add airline design
@@ -36,6 +31,9 @@ call vundle#end()            \" required
 
 filetype plugin indent on    \" required
 
-autocmd VimEnter * NERDTree | wincmd p \"auto atart nerdtree
+autocmd VimEnter * below term 
+autocmd VimEnter * wincmd k \" 생성된 터미널에 있는 커서를 편집중인 파일로 이동합니다.
+autocmd VimEnter * NERDTree | wincmd p \" nerdtree를 toggle on하고 커서를 우측(편집중인 파일)로 이동합니다.
 \" Exit Vim if NERDTree is the only window remaining in the only tab.
-autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | call feedkeys(\":quit\<CR>:\<BS>\") | endif" > ~/.vimrc
+autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | call feedkeys(\":quit\<CR>:\<BS>\") | endif
+\" .zshrc 파일의 마지막에 ls를 추가하면 목록이 출력됩니다." > ~/hello
