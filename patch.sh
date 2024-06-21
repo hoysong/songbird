28f25f850be5b259b63ff24f9870991f37376ce1
hoysong <eagleteo@naver.com>
2024-06-21 16:32:56 +0900

    fix patch.sh

--- patch.sh
+++ patch.sh
@@ -1,6 +1,9 @@
-echo "clone vundlevim"
-git clone git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "clone vundlevim..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+echo "delete .vimrc"
+rm ~/.vimrc
+echo "make vimrc file..."
 echo "if has(\"systax\")
     syntax on
 endif
@@ -14,7 +17,7 @@ set hlsearch
 set nu
 \" Enable copy to clipboard
 set clipboard=unnamed
-set termwinsize=7x200 \" vim 안에서 ':term' 명령을 통해 생성된 터미널의 크기입니다. (수직)x(수평)
+set termwinsize=8x200 \" vim 안에서 ':term' 명령을 통해 생성된 터미널의 크기입니다. (수직)x(수평)
 
 filetype plugin indent on    \" required
 set nocompatible              \" be iMproved, required
@@ -36,4 +39,4 @@ autocmd VimEnter * wincmd k \" 생성된 터미널에 있는 커서를 편집중
 autocmd VimEnter * NERDTree | wincmd p \" nerdtree를 toggle on하고 커서를 우측(편집중인 파일)로 이동합니다.
 \" Exit Vim if NERDTree is the only window remaining in the only tab.
 autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | call feedkeys(\":quit\<CR>:\<BS>\") | endif
-\" .zshrc 파일의 마지막에 ls를 추가하면 목록이 출력됩니다." > ~/hello
+\" .zshrc 파일의 마지막에 ls를 추가하면 목록이 출력됩니다." > ~/.vimrc
