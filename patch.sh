c4273237be494e7b133568339f4b3c4f74170d1e
hoysong_firebat <hoysong@student.42gyeongsan.kr>
2024-07-03 04:50:21 +0900

    fix nvm problem fix user manual

--- patch.sh
+++ patch.sh
@@ -1,28 +1,28 @@
 clear
 echo -e "\033[32m
- _____                   _     _         _ 
-/  ___|   song_bird     | |   (_)       | |
-\ \`--.  ___  _ __   __ _| |__  _ _ __ __| |
- \`--. \/ _ \| '_ \ / _\` | '_ \| | '__/ _\` |
-/\__/ / (_) | | | | (_| | |_) | | | | (_| |
-\____/ \___/|_| |_|\__, |_.__/|_|_|  \__,_|
-                    __/ |   vim_setup.sh               
-                   |___/                   
+   _____                   _     _         _ 
+  /  ___|   song_bird     | |   (_)       | |
+  \ \`--.  ___  _ __   __ _| |__  _ _ __ __| |
+   \`--. \/ _ \| '_ \ / _\` | '_ \| | '__/ _\` |
+  /\__/ / (_) | | | | (_| | |_) | | | | (_| |
+  \____/ \___/|_| |_|\__, |_.__/|_|_|  \__,_|
+                      __/ |   vim_setup.sh               
+                     |___/                   
 \033[0m"
-echo -e "\033[32mvundlevim=============\033[0m"
+echo -e "\033[32m vundlevim=============\033[0m"
 echo "install vundlevim"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "done!"
 echo ""
 
-echo -e "\033[32mvim plug==============\033[0m"
+echo -e "\033[32m vim plug==============\033[0m"
 echo "install vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "done!"
 echo ""
 
-echo -e "\033[32mvimrc_setup=================\033[0m"
+echo -e "\033[32m vimrc_setup=================\033[0m"
 echo "delete origin vimrc"
 rm ~/.vimrc
 echo "copy new vimrc"
@@ -30,22 +30,24 @@ cp .vimrc ~/
 echo "done!"
 echo ""
 
-echo -e "\033[32mnodejs_setup==========\033[0m"
+echo -e "\033[32m nodejs_setup==========\033[0m"
 echo "curl nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh ~/ | bash
 echo "done!"
+echo "Sourcing nvm..."
+. ~/.nvm/nvm.sh
+echo -n "nvm's version: "
+nvm -v
+echo "Install nodejs 16"
+nvm install 16
+echo "done!"
 echo ""
 
-echo -e "\033[32mAUTO_PATCH_DONE=======\033[0m"
-echo ""
-echo -e "\033[31m	NOTICE!!\033[0m"
+echo -e "\033[32m AUTO_PATCH_DONE=======\033[0m"
+echo -e "\033[31m	== NOTICE!! ==\033[0m"
 echo "there are 'few simple steps' left."
 
-echo -n "├─0.type \""
-echo -en "\033[32mnvm install 16\033[0m"
-echo "\" in terminal"
-
-echo "├─1. open ANY C files."
+echo "├─1. open ANY C file with vim editor. (type 'vim [your .c file]')"
 
 echo -n "├─2. Type '"
 echo -en "\033[32m:\033[0m"
